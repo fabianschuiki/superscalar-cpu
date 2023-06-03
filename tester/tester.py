7afd15cdf8a029680fbd317a818761d0dd85a533
Fabian Schuiki <fabian@schuiki.ch>
2023-06-03 12:31:14 -0700

    Adjust tester script for adder

--- tester/tester.py
+++ tester/tester.py
@@ -92,83 +92,22 @@ class Tester:
         data[1] = self.led_circle & 0xFF
         self.write_aux_chain(data)
 
-
-# Test the GPR.
-tester = Tester()
-num_mismatches = 0
-interactive = False
+    # Update the circle LEDs to show progress.
+    def set_progress_circle(self, current: int, end: int):
+        self.led_circle = (1 << (current * 8 // end + 1)) - 1
 
 
-# Apply some input signals to the GPR.
-def apply_inputs(wd: int = 0,
-                 we: bool = False,
-                 re1: bool = False,
-                 re2: bool = False,
-                 re3: bool = False):
-    # Assemble the control signals.
-    ctrl = 0
-    ctrl |= (~we & 1) << 0
-    ctrl |= (~re1 & 1) << 1
-    ctrl |= (~re2 & 1) << 2
-    ctrl |= (~re3 & 1) << 3
-
-    # Write tester outputs / GPR inputs.
-    outputs = [0] * 8
-    outputs[0] = wd  # outputs 0 to 7
-    outputs[2] = ctrl  # outputs 16 to 19
-    tester.write_output_chain(outputs)
-
-    print(
-        f"inputs:  WD={wd:08b}  WE={we:b}  RE1={re1:b}  RE2={re2:b}  RE3={re3:b}"
-    )
-
-
-# Toggle the clock of the GPR.
-def clock():
-    if interactive:
-        print("clock")
-        input()
-    tester.clocks = 0xF
-    tester.update_aux()
-    tester.clocks = 0x0
-    tester.update_aux()
-
-
-# Read and check the output signals of the GPR.
-def check_outputs(rd1: int, rd2: int, rd3: int):
-    # Read tester inputs / GPR outputs (with pullup).
-    tester.pullup = True
-    tester.update_aux()
-    inputs_pu = tester.read_input_chain(8)
-
-    # Read tester inputs / GPR outputs (with pulldown).
-    tester.pullup = False
-    tester.update_aux()
-    inputs_pd = tester.read_input_chain(8)
-
-    # Check the outputs.
-    rd1_exp, rd1_act = check_output(rd1, inputs_pu[0], inputs_pd[0])
-    rd2_exp, rd2_act = check_output(rd2, inputs_pu[1], inputs_pd[1])
-    rd3_exp, rd3_act = check_output(rd3, inputs_pu[2], inputs_pd[2])
-
-    print(
-        f"outputs: RD1={rd1_act} ({rd1_exp})  RD2={rd2_act} ({rd2_exp})  RD3={rd3_act} ({rd3_exp})"
-    )
-    if interactive:
-        input()
-
-
-# Check a single RD output against an expected value.
-def check_output(exp: int, act_pu: int, act_pd: int):
+# Check a single output against an expected value.
+def check_output(exp: int, act_pu: int, act_pd: int, width: int = 8):
     # If the expected value is `None`, we expect all bits to be Z, otherwise
-    # format the expected value as an 8 digit binary number.
-    exp_str = f"{exp:08b}" if exp is not None else "ZZZZZZZZ"
+    # format the expected value as a binary number.
+    exp_str = f"{exp:0{width}b}" if exp is not None else "Z" * width
 
-    # Format the values with pullup and pulldown as an 8 digit binary number. If
-    # both values agree on a digit, write that digit. If the values disagree on
-    # a digit, write a "Z".
-    act_pu = f"{act_pu:08b}"
-    act_pd = f"{act_pd:08b}"
+    # Format the values with pullup and pulldown as a binary number. If both
+    # values agree on a digit, write that digit. If the values disagree on a
+    # digit, write a "Z".
+    act_pu = f"{act_pu:0{width}b}"
+    act_pd = f"{act_pd:0{width}b}"
     act_str = "".join(pu if pu == pd else "Z"
                       for pu, pd in zip(act_pu, act_pd))
 
@@ -183,80 +122,49 @@ def check_output(exp: int, act_pu: int, act_pd: int):
         return exp_str, colored(act_str, "green")
 
 
-# Write a zero to the register to reset it.
-print()
-print("Check write of 0x00")
-apply_inputs(wd=0x00, we=True)
-clock()
-
-# Check that each RD port can be Z or show the zero we just wrote.
-apply_inputs(re1=True)
-check_outputs(0x00, None, None)
-apply_inputs(re2=True)
-check_outputs(None, 0x00, None)
-apply_inputs(re3=True)
-check_outputs(None, None, 0x00)
-
-# Write all ones to the register and check that we can read those back.
-print()
-print("Check write of 0xFF")
-apply_inputs(wd=0xFF, we=True)
-clock()
-apply_inputs(re1=True, re2=True, re3=True)
-check_outputs(0xFF, 0xFF, 0xFF)
+# Test the GPR.
+tester = Tester()
+num_mismatches = 0
 
 
-# Golden model for the register file. Allows us to just set the inputs, and it
-# will keep track of what we expect the register to contain in the `stored_data`
-# global variable. So if we decide to read through RE1, RE2, or RE3, it will
-# check that we indeed read what the register is supposed to store.
-def apply_and_check_golden_model(wd: int = 0,
-                                 we: bool = False,
-                                 re1: bool = False,
-                                 re2: bool = False,
-                                 re3: bool = False):
-    global stored_data
-    apply_inputs(wd, we, re1, re2, re3)
-    clock()
-    if we:
-        stored_data = wd
-    check_outputs(
-        stored_data if re1 else None,
-        stored_data if re2 else None,
-        stored_data if re3 else None,
-    )
+# Check that addition works.
+def check(lhs: int, rhs: int, ci: int):
+    # Write tester outputs / ALU inputs.
+    outputs = [0] * 8
+    outputs[0] = lhs  # outputs 0 to 7
+    outputs[2] = rhs  # outputs 16 to 23
+    outputs[4] = ci  # output 32
+    tester.write_output_chain(outputs)
+
+    # Read tester inputs / ALU outputs (with pullup).
+    tester.pullup = True
+    tester.update_aux()
+    inputs_pu = tester.read_input_chain(8)
+
+    # Read tester inputs / ALU outputs (with pulldown).
+    tester.pullup = False
+    tester.update_aux()
+    inputs_pd = tester.read_input_chain(8)
+
+    # Check the outputs.
+    value = lhs + rhs + ci
+    result_exp, result_act = check_output(value & 0xFF, inputs_pu[0],
+                                          inputs_pd[0])
+    co_exp, co_act = check_output((value >> 8) & 0x1,
+                                  inputs_pu[2] & 0x1,
+                                  inputs_pd[2] & 0x1,
+                                  width=1)
+
+    print(f"{lhs:08b} + {rhs:08b} + {ci:01b} = " +
+          f"{co_act} {result_act} ({co_exp} {result_exp})")
 
 
-# Perform a few manual checks with the golden model.
-print()
-print("Check against golden model (manual inputs)")
-stored_data = 0xFF
-apply_and_check_golden_model(re1=True)
-apply_and_check_golden_model(wd=0x55, we=False, re2=True)
-apply_and_check_golden_model(wd=0x55, we=True, re2=True)
-apply_and_check_golden_model(wd=0xF0, we=False, re3=True)
-apply_and_check_golden_model(wd=0xF0, we=True, re3=True)
-
-# Apply a few random inputs and check against the golden model.
-num_random_inputs = 250
-print()
-print("Check against golden model (random inputs)")
-for i in range(num_random_inputs):
-    tester.led_circle = (1 << (i * 8 // num_random_inputs + 1)) - 1
-    apply_and_check_golden_model(
-        wd=random.getrandbits(8),
-        we=random.getrandbits(1),
-        re1=random.getrandbits(1),
-        re2=random.getrandbits(1),
-        re3=random.getrandbits(1),
-    )
-
-# Clear back to zero for aesthetics.
-print()
-print("Clear to zero")
-apply_inputs(wd=0x00, we=True)
-clock()
-apply_inputs()
+# Randomized testing.
+num_checks = 1000
+for i in range(num_checks):
+    tester.set_progress_circle(i, num_checks)
+    check(random.getrandbits(8), random.getrandbits(8), random.getrandbits(1))
+check(0, 0, 0)
 
 # Show pass/fail on LED.
 tester.led_fail = num_mismatches > 0
